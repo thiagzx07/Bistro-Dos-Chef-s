f34b346c8da3fb7f25edba981bcb5dcd3e535a23
Thiago <thiagobonfim888@gmail.com>
2023-05-22 20:54:24 -0300

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
-
 function menu() {
     var boxmenu = document.getElementById("boxMenu");
     boxmenu.classList.toggle("aparece");
 }
+
+function acceptCookies() {
+    // Salvar a preferência do usuário de aceitar os cookies
+    localStorage.setItem('cookiesAccepted', 'true');
+
+    // Remover a notificação de cookie
+    var cookieNotification = document.querySelector('.cookie-notification');
+    cookieNotification.style.display = 'none';
+}
+
+// Verificar se o usuário já aceitou os cookies anteriormente
+window.addEventListener('load', function() {
+    var cookiesAccepted = localStorage.getItem('cookiesAccepted');
+    if (cookiesAccepted) {
+        var cookieNotification = document.querySelector('.cookie-notification');
+        cookieNotification.style.display = 'none';
+    }
+});
