fc5eb4509dc89687d82e9ca1d6402f68482cab90
Thiago <thiagobonfim888@gmail.com>
2023-05-23 22:08:35 -0300

    Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+function menu() {
+    var boxmenu = document.getElementById("boxMenu");
+    boxmenu.classList.toggle("aparece");
+    console.log("aqui");
+}
+
 function acceptCookies() {
     // Aqui você pode adicionar código para salvar a preferência do usuário em relação aos cookies
     // Por exemplo, usando cookies ou localStorage
